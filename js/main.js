c16576356108b923433a6f61e0a4cf63daacde64
Gustavo Nofoente <gustavonofoente@gmail.com>
2017-07-02 09:59:41 -0300

    Finished standard filter implementation

--- js/main.js
+++ js/main.js
@@ -3,20 +3,69 @@ import modalities from './classes.json';
 var btn = document.querySelector('.menu-btn');
 var menu = document.querySelector('.mobile-wrapper');
 var output = document.querySelector('.modalities .container');
+var filter = document.querySelectorAll('.filter input[type=checkbox]');
 
 btn.onclick = function () {
 	toggle(menu, 'mobile-wrapper--visible');
 };
 
+filter.forEach(function (value) {
+	value.addEventListener('click', function () {
+		if (this.value == 'acqua' && this.checked) {
+			var other = document.querySelector('.filter input[value=non-acqua]');
+			other.disabled = true;
+			render('acqua');
+		} else if (this.value == 'non-acqua' && this.checked) {
+			var other = document.querySelector('.filter input[value=acqua]');
+			other.disabled = true;
+			render('non-acqua');
+		} else {
+			filter[0].disabled = false;
+			filter[1].disabled = false;
+			render();
+		}
+	});
+})
+
+
 function toggle (element, className) {
 	element.classList.toggle(className);
 }
 
-(function render () {
+function hasShowcase () {
+	if (document.querySelector('li.modality')) {
+		return true;
+	}
+
+	return false;
+}
+
+function render (filter) {
+	if (hasShowcase()) {
+		var showcase = document.querySelector('.bx-wrapper');
+		showcase.remove();
+	}
+
 	var list = document.createElement('ul');
 	list.classList.add('showcase', 'bxslider');
 
 	var modsCollection = modalities.classes;
+
+	if (filter == 'acqua') {
+		modsCollection = modsCollection.filter(function (mod) {
+			if (mod.type == 'aquatic') {
+				return true;
+			}
+		});
+	}
+
+	if (filter == 'non-acqua') {
+		modsCollection = modsCollection.filter(function (mod) {
+			if (mod.type == 'non-aquatic') {
+				return true;
+			}
+		});
+	}
 
 	modsCollection.forEach(function (mod) {
 		console.log(mod);
@@ -29,14 +78,18 @@ function toggle (element, className) {
 
 	console.log(list);
 	output.appendChild(list);
-} ());
 
-$(document).ready(function () {
 	$('.bxslider').bxSlider({
 		minSlides: 1,
-		maxSlides: 4,
+		maxSlides: 8,
 		slideWidth: 300,
-		slideMargin: 20
+		slideMargin: 20,
 	});
+};
+
+
+
+$(document).ready(function () {
+	render();
 });
 
